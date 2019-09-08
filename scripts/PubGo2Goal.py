0ed87ee8668c34948da44f774371f07d80d98a0c
Matheus Nascimento <contamatheusnascimento@gmail.com>
2019-09-08 20:58:34 -0300

    Implement controller

--- scripts/PubGo2Goal.py
+++ scripts/PubGo2Goal.py
@@ -1,42 +1,73 @@
 #!/usr/bin/env python
 
-import rospy
-
+import angles as ros_angles
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Pose2D
+import rospy
 from turtlesim.msg import Pose
+
+from math import pow, atan2, sqrt
 
 
 class PubGo2Goal:
     def __init__(self):
         rospy.init_node('PubGo2Goal')
-        self.initControllerVariables()
+        self.initMemberVariables()
         self.initROSChannels()
         rospy.spin()
 
     def initROSChannels(self):
-        self.pose_listener = rospy.Subscriber("my_turtle/turtle1/pose", Pose, self.callbackPose)
-        self.goal_listener = rospy.Subscriber("my_turtle/goal", Pose2D, self.callbackGoal)
-        self.velocity_publisher = rospy.Publisher('my_turtle/turtle1/cmd_vel', Twist, queue_size=10)
+        self.pose_listener = rospy.Subscriber("turtle1/pose", Pose, self.callbackPose)
+        self.goal_listener = rospy.Subscriber("goal", Pose2D, self.callbackGoal)
+        self.velocity_publisher = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
 
-    def initControllerVariables(self):
-        self.vel_msg = Twist()
+    def initMemberVariables(self):
         self.current_pose = Pose()
 
-
     def callbackPose(self, msg):
-            self.current_pose = msg
-            print "X = " + str(self.current_pose.x) + "Y = " + str(self.current_pose.y)
+        self.current_pose = msg
 
     def callbackGoal(self, msg):
-            rospy.loginfo(rospy.get_caller_id() + "I heard Goal")
+        current_goal = msg
+        self.goToGoal(current_goal)
 
-    def goToPose(self, current_position, goal_position):
-        r = rospy.Rate(0.5)  # 10hz
-        self.vel_msg.linear.x = 10
-        self.vel_msg.linear.y = 0
-        self.vel_msg.linear.z = 0
-        self.vel_msg.angular.x = 0
-        self.vel_msg.angular.y = 0
-        self.vel_msg.angular.z = 1.55
-        rospy.spin()
+    def goToGoal(self, goal_position):
+        rospy.loginfo("Moving PUT TURTLENAME to the pose: (" + str(goal_position.x) + " , " + str(
+            goal_position.y) + ")" + " and theta = " + str(goal_position.theta))
+
+        vel_msg = Twist()
+
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+
+        distance_tolerance = 0.1
+
+        rate = rospy.Rate(5)
+
+        while self.euclideanDistance(goal_position) >= distance_tolerance:
+
+            vel_msg.linear.x = self.linearVelocity(goal_position)
+            vel_msg.angular.z = self.angularVelocity(goal_position)
+
+            self.velocity_publisher.publish(vel_msg)
+            rate.sleep()
+
+        vel_msg.linear.x = 0
+        vel_msg.angular.z = 0
+
+        self.velocity_publisher.publish(vel_msg)
+
+    def euclideanDistance(self, goal_pose):
+        return sqrt(pow((goal_pose.x - self.current_pose.x), 2) + pow((goal_pose.y - self.current_pose.y), 2))
+
+    def linearVelocity(self, goal_pose, gain=1.5):
+        return gain * self.euclideanDistance(goal_pose)
+
+    def angularVelocity(self, goal_pose, gain=4):
+        return gain * ros_angles.shortest_angular_distance(self.current_pose.theta, self.angleToTheGoal(goal_pose))
+
+    def angleToTheGoal(self, goal_pose):
+        return atan2(goal_pose.y - self.current_pose.y, goal_pose.x - self.current_pose.x)
